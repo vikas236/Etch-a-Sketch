c36b64833b658065ddc695abdf2fd79ccb504f10
sai vikas <saivikas461@gmail.com>
2023-05-30 04:53:15 +0530

    add color picker link with apply button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // calling elements
 const container = document.querySelector(".container");
 const clear = document.querySelector(".clear");
+const paint = document.querySelector("#color");
 const sqrs = document.querySelector(".sqrs");
 const apply = document.querySelector(".apply");
 
 
 // generate sketchpad and boxes
-function generateBoxes(num) {
+function generateBoxes(num, color) {
     sqrs.value = num;
     let size = (600 / num) + "px";
     for (i = 0; i < num; i++) {
@@ -17,7 +18,6 @@ function generateBoxes(num) {
             row.childNodes[k].classList.add("box");
             row.childNodes[k].style.width = size;
             row.childNodes[k].style.height = size;
-            console.log(row.childNodes[k].style.width)
         };
     };
 };
@@ -26,17 +26,19 @@ generateBoxes(100);
 
 // functions
 const box = document.querySelectorAll(".box");
+let color;
 
 function activateBox() {
+    color = paint.value;
     for (i = 0; i < box.length; i++) {
         box[i].addEventListener("mouseover", function () {
             if (draw && !this.classList.contains("active")) {
-                this.style.background = "#9B5DE5";
+                this.style.background = color;
             }
         });
         box[i].addEventListener("mousedown", function () {
             if (!this.classList.contains("active")) {
-                this.style.background = "#9B5DE5";
+                this.style.background = color;
             }
         })
     }
@@ -45,7 +47,10 @@ function activateBox() {
 function applyNumber() {
     apply.addEventListener("click", function () {
         empty();
-        if (sqrs.value <101) { generateBoxes(sqrs.value); }
+        if (sqrs.value <101) { 
+            generateBoxes(sqrs.value);
+            color = paint.value;
+        }
         else { sqrs.value = 100 };
     })
 }
@@ -61,6 +66,6 @@ window.addEventListener("mouseup", function () { draw = false; })
 
 
 // calling functions and events
-activateBox();
-clear.addEventListener("click", empty);
 applyNumber();
+activateBox(color);
+clear.addEventListener("click", empty);
